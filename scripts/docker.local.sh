f9b6b86700a4c4765b55fa88fc90dbe7fd79f081
Daniel Brai <braidaniel2001@gmail.com>
2024-03-10 01:16:42 +0100

    [Chore]: modified local build script

--- scripts/docker.local.sh
+++ scripts/docker.local.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-check_docker_installed() {
-    if ! command -v docker &> /dev/null; then
-        echo "Docker is not installed. Please install Docker before running this script."
-        exit 1
-    fi
-}
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Please install Docker before running this script."
+    exit 1
+fi
 
-start_jetei_application() {
-    echo "Starting Jetei....."
-    docker-compose -f docker-compose.local.yml up -d
-}
+cd "$(dirname "$0")/.."
+    
+echo "Starting Jetei...."
 
-main() {
-    check_docker_installed
-    start_jetei_application
-}
-
-main
+docker-compose -f  docker-compose.local.yml up $@
