6808f14c5b3050a5c95b290fd18fed78482b52b5
Daniel Brai <braidaniel2001@gmail.com>
2024-05-20 02:34:06 +0100

    [Chore]: Updated environment

---
+++ deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "========Starting deployment process========"
+
+# start ssh-agent
+eval $(ssh-agent -s)
+
+# Add the SSH key to the ssh-agent
+ssh-add ~/.ssh/jetei_dev_github
+ssh -T git@github.com
+
+echo "========Pulling new API changes========"
+
+# download new version of our application
+cd /home/ubuntu/jetei-dev
+git pull git@github.com:Daniel-Brai/Jetei.git
+
+# install all the dependencies
+echo "========Installing dependencies========"
+pnpm install
+
+# perform migrations
+echo "========Sync database========"
+pnpm run prisma:migrate
+
+# build the application
+echo "========Building the application========"
+pnpm run build:css
+pnpm run build
+
+# run the application
+echo "=====Starting the application========="
+cd ~
+pm2 start jetei --watch
+pm2 save --force
+
+echo "✨ Deployment process completed"
--- docker.sh
+++ docker.sh
@@ -4,9 +4,7 @@ if ! command -v docker &> /dev/null; then
     echo "Docker is not installed. Please install Docker before running this script."
     exit 1
 fi
-
-cd "$(dirname "$0")/.."
-    
+ 
 echo "Starting Jetei...."
 
 docker-compose up -d
